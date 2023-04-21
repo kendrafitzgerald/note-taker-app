3494fc1fe12970449147f0807fec867b4cad7684
Kendra Fitzgerald <kendrajfitzgerald@gmail.com>
2023-04-20 20:05:50 -0400

    added comments to code

--- server.js
+++ server.js
@@ -1,42 +1,46 @@
+//imported express
 const express = require('express');
+//imported unique id npm
 const uuid = require('uuid');
+//imported database
 const notes = require('./db/db.json');
+//imported pathing
 const path = require('path')
+//imported file system
 const fs = require('fs');
 
-
+//created port to be readable by heroku
 const PORT = process.env.PORT || 3001;
-
+//makes express a variable
 const app = express();
-
+//middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
 app.use(express.static('public'));
 
-
+//get request for notes html
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-
+//get request for api notes
 app.get('/api/notes', (req, res) =>
     fs.readFile('./db/db.json', 'utf-8', (err, data) => {
         err ? console.error(err) : res.json(JSON.parse(data))
     })
 );
-
+//post requests for notes
 app.post('/api/notes', (req, res) => {
 
  const {title, text} = req.body;
-
+//creates a new note with a unique id
  if (title && text) {
     const newNote = {
         title,
         text,
         id: uuid.v4(),
     };
-
+//adds new notes to notes array
     notes.push(newNote);
 
     fs.writeFile(`./db/db.json`, JSON.stringify(notes), (err) =>
@@ -52,11 +56,9 @@ app.post('/api/notes', (req, res) => {
     res.status(500).json('Need note title and note text')
  };
  });
-
+//delete request for notes
  app.delete("/api/notes/:id", (req, res) => {
     const noteId = req.params.id
-    console.log(`ID: ${noteId}`)
-
     fs.readFile("./db/db.json", "utf-8", (err, data) => {
         if (err) {
             throw err
@@ -74,11 +76,11 @@ app.post('/api/notes', (req, res) => {
         });
     });    
 });
-
+//get request for public html, wildcard
  app.get('*', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
-
+//listen request to launch app in local host
  app.listen(PORT, () =>
     console.log(`Listening at http://localhost:${PORT}`)
  );
